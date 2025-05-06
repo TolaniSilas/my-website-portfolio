5a0437eb8008ef0486efd044bc5eee1208e35148
TolaniSilas <tolanisilas3606@gmail.com>
2025-05-06 03:31:05 -0700

    set up the site routes.

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Blog from './pages/Blog';
+import Research from './pages/Research';
+import NoPage from "./pages/NoPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/research" element={<Research />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </Router>
+  )
+
 }
 
 export default App;
